88fc9a3a22d00dee46eb420915592670ebd63e4b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-15 14:47:58 +0300

    done

---
+++ app/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  )
+}
+
+export default profile
+
+const styles = StyleSheet.create({})
