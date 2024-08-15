8d624a8818e009e9a65ee71e09a84a4cbd4a033d
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-15 17:45:40 +0300

    done

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { Tabs, Redirect } from 'expo-router';
+
+const TabsLayout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen/>
+    </Tabs>
+  )
+}
+
+export default TabsLayout;
