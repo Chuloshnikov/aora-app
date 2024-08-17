36e694593c84bb9636d2b36e2ba07b227568fa53
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-17 20:14:43 +0300

    done

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
@@ -7,9 +7,31 @@ import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '@/components/EmptyState';
+import { getAllPosts } from '../../lib/appwrite';
 
 const Home = () => {
-  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [refreshing, setRefreshing] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await getAllPosts();
+        setData(response);
+      } catch (error: any) {
+        Alert.alert('Error', error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+}, []);
+
+console.log(data);
 
   const onRefresh = () => {
     setRefreshing(true);
