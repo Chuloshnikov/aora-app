ed8f34f49315a86c512e4a0f6359b0edacd7f914
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-17 18:12:10 +0300

    done

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,6 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
 
 const Home = () => {
   return (
@@ -41,9 +42,14 @@ const Home = () => {
                   <Text className='text-gray-100 text-lg font-pregular mb-3'>
                       Latest Videos
                   </Text>
+
+                  <Trending posts={[{id: 1}, {id: 2}, {id: 3}] ?? []}/>
               </View>
           </View>
         )}
+        ListEmptyComponent={() => {
+          <Text className='text-white'>Empty</Text>
+        }}
       />
     </SafeAreaView>
   )
