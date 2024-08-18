c075dd63469a6df92f3e99384b62ee6d377a21b3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-18 12:37:02 +0300

    done

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,7 +1,11 @@
 import { Text, FlatList, TouchableOpacity, ImageBackground, Image, ViewToken } from 'react-native';
 import React, { useState, useCallback, useRef } from 'react';
 import * as Animatable from 'react-native-animatable';
+
+
 import { icons } from "../constants";
+
+import { Video, ResizeMode } from 'expo-av';
 
 const zoomIn = {
   0: {
@@ -39,7 +43,18 @@ const TrendingItem: React.FC<TrendingItemProps> = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+        source={{ uri: item.video }}
+        className='w-52 h-72 rounded-[35px] mt-3 bg-white/10'
+        resizeMode={ResizeMode.CONTAIN}
+        useNativeControls
+        shouldPlay
+        onPlaybackStatusUpdate = {(status) => {
+          if(status.didJustFinish) {
+            setPlay(false);
+          }
+        }}
+        />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
